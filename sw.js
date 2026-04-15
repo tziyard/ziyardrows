9bd015767fc5de9985dbc9c849d760f42ed35372
tziyard <tziyard@gmail.com>
2026-04-15 15:07:07 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,12 @@
 const CACHE = 'l135-v1';
-const FILES = [
-  '/ziyardrows/',
-  '/ziyardrows/index.html',
-  '/ziyardrows/icon.png'
-];
 
-self.addEventListener('install', e =>
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)))
-);
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  clients.claim();
+});
 
 self.addEventListener('fetch', e =>
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
