b87cb17f8ff941a3ec5b061a503743fed0f52ada
tziyard <tziyard@gmail.com>
2026-04-15 14:27:17 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,10 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker installed");
-});
+const CACHE = 'v1';
+const FILES = ['/', '/index.html', '/style.css', '/script.js'];
+
+self.addEventListener('install', e =>
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)))
+);
+
+self.addEventListener('fetch', e =>
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
+);
