d41931d63989b6efcf98f46411cf443417a6cd16
tziyard <tziyard@gmail.com>
2026-04-15 16:15:17 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,6 +8,10 @@ self.addEventListener('activate', e => {
   clients.claim();
 });
 
-self.addEventListener('fetch', e =>
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
-);
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(r => r || fetch(e.request))
+      .catch(() => caches.match('/ziyardrows/'))
+  );
+});
